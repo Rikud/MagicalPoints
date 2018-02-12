8d408538acd94881ea9d81fa1280af7c8a144cb8
Rikud <ivan_nemshilov@mail.ru>
2018-02-12 03:14:42 -0800

    Port magic

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 console.log('My first node app');
 
+
+const PORT = process.env.PORT || 8080
 const http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8080);
+}).listen(PORT, () => console.log(`Listening on ${ PORT }`));
