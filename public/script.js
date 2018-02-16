b632b9e5535850d8f8a50538f9db240b9f60fc48
Rikud <ivan_nemshilov@mail.ru>
2018-02-16 02:07:24 -0800

    Start frontend

--- public/script.js
+++ public/script.js
@@ -1,8 +1,18 @@
-const text = window.document.getElementById('text');
+const text = window.document.getElementById('SignUp');
 console.log(text);
 
-const button = window.document.getElementById('button');
+const NewGameButton = window.document.getElementById('StartGame');
+const SignUpButton = window.document.getElementById('SignUp');
+const SignInButton = window.document.getElementById('SignIn');
 
-button.addEventListener('click', () => {
-	text.innerText = 'Welcome to Frontend';
-})
+NewGameButton.addEventListener('click', () => {
+	document.location.href = './GameSettings.html';
+});
+
+SignUpButton.addEventListener('click', () => {
+	document.location.href = './SignUp.html';
+});
+
+SignInButton.addEventListener('click', () => {
+	document.location.href = './SignIn.html';
+});
