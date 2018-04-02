c7d1a39f47415a084af5eecda7952fe5b7b10497
Rikud <ivan_nemshilov@mail.ru>
2018-04-02 05:21:33 -0700

    nenew design

--- public/about/about.js
+++
@@ -1,5 +0,0 @@
-const aboutBackButton = document.getElementsByClassName('about-back-button')[0];
-
-aboutBackButton.addEventListener('click', () => {
-  document.location.href = '../menu/index.html';
-});
--- public/login/login.js
+++
@@ -1,5 +0,0 @@
-const BackButton = window.document.getElementById('Back');
-
-BackButton.addEventListener('click', () => {
-	document.location.href = '../menu/index.html';
-});
